c06f33b2d470d4da034da797dd1b7a58300bbb2e
Soprano <kula1223@gmail.com>
2019-07-27 07:57:35 +0200

    Dodaj pliki projektów.

---
+++ Baza/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Baza.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
