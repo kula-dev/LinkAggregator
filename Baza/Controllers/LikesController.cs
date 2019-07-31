7e4133ff6a020a6bc590af1aae14b42003ad712c
Soprano <kula1223@gmail.com>
2019-07-31 18:21:48 +0200

    Zabezpieczenia do lajkowania

--- Baza/Controllers/LikesController.cs
+++ Baza/Controllers/LikesController.cs
@@ -22,6 +22,7 @@ namespace Baza.Controllers
         // POST: Likes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
         public async Task<IActionResult> Like([Bind("LikeId,UserID,LinkID")] Likes likes, int id)
         {
             likes.LinkID = id;
